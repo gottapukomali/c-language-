62530d8c03cb96b0014ba383799475fb317878e9
gottapukomali <152802967+gottapukomali@users.noreply.github.com>
2023-12-03 22:23:39 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+  char name[10] = "india";
+  float dollar= '$';
+  int gdp = 2.2;
+  int year = 2005; 
+  
+  printf("%s\n%f\n%d\n%d",name,dollar,gdp,year);
+  
+     return 0;
+}
